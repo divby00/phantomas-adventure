dee0af46e4adcebfb2fccbd61f99bd9967fbdcf0
Division By 0 <divisionby0x00@gmail.com>
2021-11-09 06:36:07 +0100

    Some work on dialog system

---
+++ scripts/Characters.gd
@@ -0,0 +1,133 @@
+extends Node
+
+enum CharNames {
+	Phantomas,
+	Unknown,
+	Lorenzo,
+	TioJuan,
+	TioRamon,
+	TiaSimona,
+	Agustin,
+	Maximina,
+	Heladia,
+	TioEleuterio,
+	TioCalixto,
+	TioValentin,
+	DonFrancisco,
+	Flora,
+	Felipa,
+	TioVenancio,
+	TiaSusana,
+	DomingueroPadre,
+	DomingueraMadre,
+	DomingueroHijo,
+	DomingueraHija
+}
+
+var Characters = {
+	CharNames.Phantomas: {
+		"name": "Phantomas",
+		"portrait": preload("res://resources/sprites/portrait-phantomas.png"),
+		"background": preload("res://resources/sprites/frame-bg-blue.png")
+	},
+	CharNames.Unknown: {
+		"name": "Desconocido",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.Lorenzo: {
+		"name": "Lorenzo",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.TioJuan: {
+		"name": "Tio Juan",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.TioRamon: {
+		"name": "Tio Ramón",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.TiaSimona: {
+		"name": "Tia Simona",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")		
+	},
+	CharNames.Agustin: {
+		"name": "Agustín",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")				
+	},
+	CharNames.Maximina: {
+		"name": "Maximina",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")				
+	},
+	CharNames.Heladia: {
+		"name": "Heladia",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")		
+	},
+	CharNames.TioEleuterio: {
+		"name": "Tio Eleuterio",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")	
+	},
+	CharNames.TioCalixto: {
+		"name": "Tio Calixto",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.TioValentin: {
+		"name": "Tio Valentín",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.DonFrancisco: {
+		"name": "Don Francisco",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.Flora: {
+		"name": "Flora",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.Felipa: {
+		"name": "Felipa",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.TioVenancio: {
+		"name": "Tio Venancio",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.TiaSusana: {
+		"name": "Tia Susana",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.DomingueroPadre: {
+		"name": "Dominguero Padre",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.DomingueraMadre: {
+		"name": "Dominguera Madre",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.DomingueroHijo: {
+		"name": "Dominguero Hijo",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+	CharNames.DomingueraHija: {
+		"name": "Dominguera Hija",
+		"portrait": preload("res://resources/sprites/portrait-unknown.png"),
+		"background": preload("res://resources/sprites/frame-bg-brown.png")
+	},
+}
