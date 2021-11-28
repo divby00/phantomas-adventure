408a623b3d238251db97fcd8f374eb2a7fb265ae
divby00 <divisionby0x00@gmail.com>
2021-11-28 22:17:24 +0100

    Some work on player movement

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -1,35 +1,78 @@
 extends KinematicBody2D
 
 
+onready var sprite = $Sprite
 onready var animation_player = $AnimationPlayer
 
-export (int) var speed = 120
-export (int) var jump_speed = -180
-export (int) var gravity = 90
+const GRAVITY: int = 200
+const FRICTION: float = .5
+const MAX_SPEED: int = 48
+const ACCELERATION: int = 512
+const JUMP_FORCE: float = 144.0
 
-var velocity = Vector2.ZERO
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	animation_player.play("fall")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+var jumping: bool = false
+var motion: Vector2 = Vector2.ZERO
 
 
 func _physics_process(delta):
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
-	if velocity.y == 0:
-		animation_player.play("walk")
+	var input_vector: Vector2 = _get_input_vector()
+	_apply_horizontal_force(input_vector, delta)
+	_apply_friction(input_vector)
+	_jump_check(input_vector, delta)
+	_apply_gravity(delta)
+	_update_animation(input_vector)
+	motion = move_and_slide(motion, Vector2.UP)
 
-#	if Input.is_action_just_pressed("jump"):
-#		if is_on_floor():
-#			velocity.y = jump_speed
+
+func _get_input_vector():
+	var input_vector: Vector2 = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	return input_vector
+
+
+func _apply_horizontal_force(input_vector, delta):
+	if input_vector.x != 0:
+		motion.x += input_vector.x * ACCELERATION * delta
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+
+
+func _apply_friction(input_vector):
+	if input_vector.x == 0:
+		motion.x = lerp(motion.x, 0, FRICTION)
+
+
+func _apply_gravity(delta):
+	motion.y += GRAVITY * delta
+	motion.y = min(motion.y, JUMP_FORCE)
+
+
+func _update_animation(input_vector):
+	if input_vector.x != 0:
+		sprite.scale.x = sign(input_vector.x)
+
+	if is_on_floor():
+		if input_vector.x != 0:
+			animation_player.play("walk")
+		else:
+			animation_player.play("stand")
+	else:
+		if jumping:
+			animation_player.play("jump")
+		else:
+			animation_player.play("fall")
+			motion.x = 0
+
+
+func _jump_check(_input_vector, _delta):
+	if is_on_floor():
+		jumping = false
+		if Input.is_action_just_pressed("Up"):
+			jumping = true
+			motion.y = -JUMP_FORCE
+		if Input.is_action_just_pressed("Down"):
+			jumping = true
+			motion.y = -(JUMP_FORCE/3) * 2
+	else:
+		if Input.is_action_just_released("Up") and motion.y < -JUMP_FORCE/2:
+			jumping = true
+			motion.y = -JUMP_FORCE/2
