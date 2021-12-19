e765f2aad4b72d5fd092388322fbe0ffad7b64b6
divby00 <divisionby0x00@gmail.com>
2021-12-19 10:44:08 +0100

    Some work on the platforms, on the parallax background and the hit boxes of the enemies

--- scenes/Enemies/Enemy.gd
+++ scenes/Enemies/Enemy.gd
@@ -1,3 +1,12 @@
 extends KinematicBody2D
 
 
+onready var hitbox: Area2D = $Hitbox
+
+
+func _ready() -> void:
+	Utils.connect_signal(hitbox, "collision", self, "_on_hitbox_collision")
+
+
+func _on_hitbox_collision(damage):
+	PlayerStats.health -= damage
