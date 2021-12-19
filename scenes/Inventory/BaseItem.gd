79ef847de1e74320d4fccc36810af45f7f226835
divby00 <divisionby0x00@gmail.com>
2021-12-19 21:30:16 +0100

    Added energy capsules. Some sprites changed.

---
+++ scenes/Inventory/BaseItem.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+signal on_item(item)
+
+export var item_name: String
+
+var collision: bool = false
+
+
+func _process(_delta: float) -> void:
+	if collision:
+		emit_signal("on_item", self)
+
+
+func _on_BaseItem_body_entered(_body: Node) -> void:
+	collision = true
+
+func _on_BaseItem_body_exited(_body: Node) -> void:
+	collision = false
+
