7fa04e26dab4167f3b448d8619522b4e595fa90c
divby00 <divisionby0x00@gmail.com>
2022-01-22 22:59:33 +0100

    Cleaning the tools folder

--- tools/extract_tileset.py
+++ tools/extract_tileset.py
@@ -1,7 +1,49 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+from typing import Dict
+from PIL import Image
+from argparse import ArgumentParser
+
+
+class ArgsParser(object):
+    def __init__(self) -> None:
+        super().__init__()
+        self._parser = ArgumentParser()
+        self._parser.add_argument("-i", "--input", required=True, type=str, help="PNG files to extract the tileset")
+        self._parser.add_argument("-o", "--output", required=True, type=str, help="PNG output file")
+        self._args = self._parser.parse_args()
+
+    @property
+    def input(self):
+        return [file.strip() for file in self._args.input.split(',')]
+
+    @property
+    def output(self):
+        return self._args.output
+
+
+class TilesetExtractor(object):
+    def __init__(self, input_files) -> None:
+        self._input_files = input_files
+        self._read_images()
+
+    def _read_images(self):
+        pictures: Dict = {}
+        for f in self._input_files:
+            try:
+                img = Image.open(f)
+                pictures.update(f, img)
+            except:
+                print("The file {} doesn't exist".format(f))
+        if len(pictures) == 0:
+            raise Exception("File not found")
+        else:
+            print("Ok")
+
 
 def main():
-    pass
+    args_parser = ArgsParser()
+    tileset_extractor = TilesetExtractor(args_parser.input)
 
 if __name__ == '__main__':
     main()
